499cc2a4684264bf8df4d7b5a16f3b320800f741
Will Taylor <will.ej.taylor@gmail.com>
2024-03-24 14:00:03 +0000

    add window customisation + logging to app init

--- src-tauri/src/extractors/pdf.rs
+++ src-tauri/src/extractors/pdf.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 
-pub fn read_pdf(path: &Path) -> anyhow::Result<String> {
+pub fn read_pdf<P: AsRef<Path>>(path: P) -> anyhow::Result<String> {
     // pdf_extract likes to panic a lot
+    let path = path.as_ref();
     std::panic::catch_unwind(|| pdf_extract::extract_text(path).map_err(|e| e.into()))
         .map_err(|e| anyhow::anyhow!("pdf_extract panicked: {:?}", e))?
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,15 @@
 mod args;
 mod commands;
 mod extractors;
+mod logging;
 mod matcher;
 mod utils;
 mod worker;
 use commands::*;
-use tauri::Manager;
 
 fn main() {
+    logging::init();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             open_in_explorer,
@@ -18,9 +20,48 @@ fn main() {
             preview_file
         ])
         .setup(|app| {
-            utils::set_platform(app);
+            let handle = app.handle();
+            let window = create_window(&handle)?;
+
+            #[cfg(debug_assertions)]
+            window.open_devtools();
+
+            utils::set_platform(&window);
+
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[cfg(not(target_os = "macos"))]
+fn create_window(handle: &tauri::AppHandle) -> anyhow::Result<tauri::Window> {
+    let app_title = handle.package_info().name.clone();
+    let window =
+        tauri::WindowBuilder::new(handle, "main", tauri::WindowUrl::App("index.html".into()))
+            .resizable(true)
+            .title(app_title)
+            .disable_file_drop_handler()
+            .min_inner_size(800.0, 600.0)
+            .inner_size(1440.0, 810.0)
+            .build()?;
+    tracing::info!("Window created");
+    Ok(window)
+}
+
+#[cfg(target_os = "macos")]
+fn create_window(handle: &tauri::AppHandle) -> anyhow::Result<tauri::Window> {
+    let app_title = handle.package_info().name.clone();
+    let window =
+        tauri::WindowBuilder::new(handle, "main", tauri::WindowUrl::App("index.html".into()))
+            .resizable(true)
+            .title(app_title)
+            .min_inner_size(800.0, 600.0)
+            .inner_size(1440.0, 810.0)
+            .hidden_title(true)
+            .disable_file_drop_handler()
+            .title_bar_style(tauri::TitleBarStyle::Overlay)
+            .build()?;
+    tracing::info!("Window created");
+    Ok(window)
 }
