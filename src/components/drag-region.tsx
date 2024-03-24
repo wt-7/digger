1254f37805498ca1264972d24dc4fbf79a99efe0
Will Taylor <will.ej.taylor@gmail.com>
2024-03-24 22:57:45 +0000

    increase drag region z-index

--- src/components/drag-region.tsx
+++ src/components/drag-region.tsx
@@ -1,3 +1,3 @@
 export function DragRegion() {
-  return <div className="h-7 w-full fixed top-0" data-tauri-drag-region />;
+  return <div className="h-7 w-full fixed top-0 z-50" data-tauri-drag-region />;
 }
